6966e93cc329a30d9f9db12602979cd3bacbd046
zjy <irrigator@yeah.net>
2018-02-27 16:27:45 +0800

    Change host

--- config/prod.exs
+++ config/prod.exs
@@ -27,7 +27,7 @@ config :plate_slate, PlateSlateWeb.Endpoint,
   # http: [port: {:system, "PORT"}], # Uncomment this line if you are running Phoenix 1.2
   server: true, # Without this line, your app will not start the web server!
   secret_key_base: "${SECRET_KEY_BASE}",
-  url: [host: "example.com", port: 80],
+  url: [host: "lotuseater.club", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :plate_slate, PlateSlate.Repo,
